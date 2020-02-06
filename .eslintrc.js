ad2e4de8ab77f27774e89a2af6066cecf19dd80a
Jason Barry <barryofguilder@gmail.com>
2020-02-05 19:08:07 -0500

    Add Tailwind CSS

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
         '.template-lintrc.js',
         'ember-cli-build.js',
         'testem.js',
+        'app/tailwind.config.js',
         'blueprints/*/index.js',
         'config/**/*.js',
         'lib/*/index.js',
